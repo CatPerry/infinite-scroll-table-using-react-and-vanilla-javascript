8fa1b0a956ea78cfe798334e62d91e7e4de194d4
Cat Perry <000.perry@gmail.com>
2020-06-29 21:23:53 -0400

    Pull out table elements to lazy load and sort more accurately

--- src/components/TableContents.jsx
+++ src/components/TableContents.jsx
@@ -1,147 +1,139 @@
-import React, { Component } from "react";
-import ColumnResizer from "react-column-resizer";
-import ReactDOM from "react-dom";
-import { orderBy } from "lodash";
+import React, { Component } from 'react';
+import ColumnResizer from './reactColumnResizerRefactor'; // 'react-column-resizer';
+import { orderBy } from 'lodash';
 
 import './TableContents.css';
 
 export class TableContents extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      people: [],
-      sortParams: {
-        direction: 'asc'
-      }
-    };
-  }
-  currentScrollPosition = 0;
+		this.state = {
+			people: [],
+			sortParams: {
+				direction: 'asc',
+			},
+		};
+	}
+	currentScrollPosition = 0;
 
-  componentDidMount() {
-    this.lazyLoadRow();
-  }
+	componentDidMount() {
+		this.lazyLoadRows();
+	}
 
-  /** Lazy loading */
-  intersectionCallback = async (entries, observer) => {
-    let data;
+	/** Lazy loading */
+	intersectionCallback = async (entries, observer) => {
+		let data;
 
-    try {
-      let response = await fetch(`https://randomuser.me/api/?page=${this.currentScrollPosition + 1}&results=20`);
-      data = await response.json();
-    } catch (err) {
-      console.log(err);
-    }
+		try {
+			let response = await fetch(
+				`https://randomuser.me/api/?page=${this.currentScrollPosition + 1}&results=10`,
+			);
+			data = await response.json();
+		} catch (err) {
+			console.log(err);
+		}
 
-    this.setState({ people: orderBy([...this.state.people, ...data.results], ['name.first'], [this.state.sortParams.direction]) });
+		this.setState({
+			people: orderBy(
+				[...data.results, ...this.state.people],
+				['name.first'],
+				[this.state.sortParams.direction],
+			),
+		});
 
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        // append the current row in visible area
-        const divToAppendReactElement = document.createElement('div');
-        divToAppendReactElement.id = `dom${this.currentScrollPosition}`;
-        const newElement = ReactDOM.render(this.renderTableElements([...this.state.people]), divToAppendReactElement);
-        const parentNode = entry.target.parentNode;
-        const target = document.getElementById(`end-table-${this.currentScrollPosition}`);
-        // append the element just before the sentinel
-        parentNode.insertBefore(newElement, target);
-        //unobserve the current threshold
-        observer.unobserve(entry.target);
-        // set the new intersection observers
-        this.currentScrollPosition = this.currentScrollPosition + 1;
-        target.id = `end-table-${this.currentScrollPosition}`;
-        this.lazyLoadRow();
-      }
-    });
-  };
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				const target = document.getElementById(`end-table-${this.currentScrollPosition}`);
+				//unobserve the current threshold
+				observer.unobserve(entry.target);
+				// set the new intersection observers
+				this.currentScrollPosition = this.currentScrollPosition + 1;
+				target.id = `end-table-${this.currentScrollPosition}`;
+				this.lazyLoadRows();
+			}
+		});
+	};
 
-  lazyLoadRow() {
-    const observerOptions = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 1.0
-    };
+	lazyLoadRows() {
+		const observerOptions = {
+			root: null,
+			rootMargin: '0px',
+			threshold: 1.0,
+		};
 
-    const target = document.getElementById(`end-table-${this.currentScrollPosition}`);
-    const observer = new IntersectionObserver(this.intersectionCallback, observerOptions);
-    observer.observe(target);
-  }
+		const target = document.getElementById(`end-table-${this.currentScrollPosition}`);
+		const observer = new IntersectionObserver(this.intersectionCallback, observerOptions);
+		observer.observe(target);
+	}
 
-  /** Sorting */
-  handleColumnHeaderClick = (sortKey) => {
-    const {
-      sortParams: { direction }
-    } = this.state;
+	/** Sorting */
+	handleColumnHeaderClick = (sortKey) => {
+		const {
+			sortParams: { direction },
+		} = this.state;
 
-    // Toggle direction
-    const sortDirection = direction === "desc" ? "asc" : "desc";
-    // Sort people  
-    let sortedPeople = orderBy([...this.state.people], [sortKey], [sortDirection]);
-    //Update component state with new data
-    this.setState({
-      people: sortedPeople,
-      sortParams: {
-        direction: sortDirection
-      }
-    });
-  };
+		// Toggle direction
+		const sortDirection = direction === 'desc' ? 'asc' : 'desc';
+		// Sort people
+		let sortedPeople = orderBy(this.state.people, [sortKey], [sortDirection]);
+		//Update component state with new data
+		this.setState({
+			people: sortedPeople,
+			sortParams: {
+				direction: sortDirection,
+			},
+		});
+	};
 
-  /** Renders */
-  renderTableRows = (data = this.state.people) => {
-    return [...data].map(person => {
-      return (
-        <tr key={person.login.sha1}>
-          <td>{person.name.first}</td>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <td>{person.phone}</td>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <td>{person.email}</td>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <td>{person.dob.age}</td>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <td><img className="image" src={person.picture.thumbnail} alt="random user" /></td>
-        </tr>
-      );
-    });
-  };
+	/** Renders */
+	renderTableHeader = () => {
+		return (
+			<ul className='TableHeader'>
+				<li className='SortByName' onClick={() => this.handleColumnHeaderClick('name.first')}>
+					Name
+				</li>
+				<ColumnResizer className='col-resizer' minWidth={0} />
+				<li>Phone</li>
+				<ColumnResizer className='col-resizer' minWidth={0} />
+				<li>Email</li>
+				<ColumnResizer className='col-resizer' minWidth={0} />
+				<li>Age</li>
+				<ColumnResizer className='col-resizer' minWidth={0} />
+				<li>Image</li>
+			</ul>
+		);
+	};
 
-  renderTableHeader = () => {
-    return (
-      <thead className='TableHeader'>
-        <tr>
-          <th className='SortByName' onClick={() => this.handleColumnHeaderClick('name.first')}>Name</th>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <th>Phone</th>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <th>Email</th>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <th>Age</th>
-          <ColumnResizer className='col-resizer' minWidth={0} />
-          <th>Image</th>
-        </tr>
-      </thead>
-    );
-  };
+	renderTableRows = (data = this.state.people) => {
+		return data.map((person) => {
+			return (
+				<div key={person.login.sha1} className='TableRow'>
+					<span className='name'>{person.name.first}</span>
+					<ColumnResizer className='col-resizer' minWidth={0} />
+					<span className='number'>{person.phone}</span>
+					<ColumnResizer className='col-resizer' minWidth={0} />
+					<span className='email'>{person.email}</span>
+					<ColumnResizer className='col-resizer' minWidth={0} />
+					<span className='age'>{person.dob.age}</span>
+					<ColumnResizer className='col-resizer' minWidth={0} />
+					<span className='image'>
+						<img className='image' src={person.picture.thumbnail} alt='random user' />
+					</span>
+				</div>
+			);
+		});
+	};
 
-  renderTableElements = (data = this.state.people) => {
-    return (
-      <>
-        <table className={this.currentScrollPosition}>
-          {data.length > 0 && this.currentScrollPosition < 1 ? this.renderTableHeader() : null}
-          <tbody>
-            {this.renderTableRows(data)}
-          </tbody>
-        </table>
-      </>);
-  };
-
-  render() {
-    return (
-      <div className="container">
-        <div id="end-table-0"></div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className='container'>
+				{this.renderTableHeader()}
+				{this.renderTableRows()}
+				<div id='end-table-0'></div>
+			</div>
+		);
+	}
 }
 
 export default TableContents;
---
+++ src/components/reactColumnResizerRefactor/index.js
@@ -0,0 +1,5 @@
+// Original Author: Nik M, modified by Cat Perry
+// https://github.com/nik-m2/react-column-resizer
+// https://github.com/catperry
+
+module.exports = require('./columnResizer.js');
